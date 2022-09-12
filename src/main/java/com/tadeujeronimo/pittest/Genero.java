319ece449b97a4b2a2248791d7dd2c7124b43e92
Tadeu Jerônimo <tadeus.jeronimo@gmail.com>
2022-09-12 01:52:04 -0300

    Initial commit

---
+++ src/main/java/com/tadeujeronimo/pittest/Genero.java
@@ -0,0 +1,7 @@
+package com.tadeujeronimo.pittest;
+
+public enum Genero {
+	 DRAMA,
+	 ROMANCE,
+	 COMEDIA,
+}
---
+++ src/main/java/com/tadeujeronimo/pittest/Main.java
@@ -0,0 +1,12 @@
+package com.tadeujeronimo.pittest;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//		Filme f3 = new Filme("Java muito feliz", Genero.ROMANCE);
+//		Locacao locacao = new Locacao();
+
+	}
+
+}
